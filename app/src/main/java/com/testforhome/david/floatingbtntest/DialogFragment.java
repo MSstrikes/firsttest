ab6e23b2ec88f3ca8411d0f9357d25a8577e980f
MSstrikes <sd85577203@gmail.com>
2016-10-11 22:37:27 +0800

    初次提交

---
+++ app/src/main/java/com/testforhome/david/floatingbtntest/DialogFragment.java
@@ -0,0 +1,40 @@
+package com.testforhome.david.floatingbtntest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2016/10/10 0010.
+ */
+public class DialogFragment extends android.app.DialogFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Light_NoTitleBar_Fullscreen);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialogfullscreen_main,null);
+        Button btn = (Button) rootView.findViewById(R.id.btn_close);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void dismiss() {
+        System.out.println("Dialog Dismiss");
+        super.dismiss();
+    }
+}
