b5715f18600fec44c2da3af9e6d8d1cc186fe521
MSstrikes <sd85577203@gmail.com>
2016-11-09 00:55:49 +0800

    初次提交

--- app/src/main/java/com/testforhome/david/floatingbtntest/ControlFragment.java
+++ app/src/main/java/com/testforhome/david/floatingbtntest/ControlFragment.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
